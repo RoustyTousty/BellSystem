7a24df75b1e305fd7c5d58ac170cd589268560eb
RoustyTousty <emils.misisons@gmail.com>
2024-10-31 16:01:39 +0200

    Project setup

---
+++ main/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let appServe;
+
+if (app.isPackaged) {
+  import("electron-serve").then((module) => {
+    appServe = module.default({ directory: path.join(__dirname, "../out") });
+  });
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (app.isPackaged) {
+    appServe(win).then(() => {
+      win.loadURL("app://-");
+    });
+  } else {
+    win.loadURL("http://localhost:3000");
+    win.webContents.openDevTools();
+    win.webContents.on("did-fail-load", (e, code, desc) => {
+      win.webContents.reloadIgnoringCache();
+    });
+  }
+};
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
